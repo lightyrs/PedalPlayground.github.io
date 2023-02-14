6ed57857dd1bd985ebd5d1ea0283d5c480c8adbe
Harris Novick <harrisdavidnovick@gmail.com>
2023-02-14 01:17:44 -0500

    Add pp3+, ppX8, kenton thru-5

--- gulpfile.js
+++ gulpfile.js
@@ -117,7 +117,7 @@ gulp.task("scripts", function () {
 
 gulp.task("process-images", function () {
   return gulp
-    .src("app/images/pedals/*.png")
+    .src("app/images/pedals-new/*.png")
     .pipe(cache("images"))
     .pipe(
       rezzy([{
